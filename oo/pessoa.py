76bd07a6e6c1ca93f28dbe465115784fbe3b4281
Micro do Guto <gustavoguesser@gmail.com>
2021-05-28 19:58:09 -0300

    Criado Metodo Comprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,3 +1,10 @@
 class Pessoa:
-    pass
+    def comprimentar(self):
+        return f'Ola {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.comprimentar(p))
+    print(id(p))
+    print(p.comprimentar())
 
